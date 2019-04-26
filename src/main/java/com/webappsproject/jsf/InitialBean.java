a51f1f5539cd19edb0a123d024c4b89b88150a2f
emrepun <emrehavan@hotmail.com>
2019-04-26 16:08:44 +0100

    Respective login pages and redirections made for admin, supervisor and student.
    Logout functionality is not working, still requires refactoring.

--- src/main/java/com/webappsproject/jsf/InitialBean.java
+++ src/main/java/com/webappsproject/jsf/InitialBean.java
@@ -11,7 +11,12 @@ import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.ejb.Singleton;
 import javax.enterprise.context.RequestScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 import javax.inject.Named;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 /**
  *
@@ -37,4 +42,17 @@ public class InitialBean implements Serializable {
     public String loginSelected() {
         return "login";
     }    
+    
+    public void logout() {
+        FacesContext context = FacesContext.getCurrentInstance();
+        HttpServletRequest request = (HttpServletRequest) context.getExternalContext().getRequest();
+        try {
+            //this method will disassociate the principal from the session (effectively logging him/her out)
+            request.logout();
+            request.getSession(true).invalidate();
+            context.addMessage(null, new FacesMessage("User is logged out"));
+        } catch (ServletException e) {
+            context.addMessage(null, new FacesMessage("Logout failed."));
+        }
+    }
 }
--- target/generated-sources/annotations/com/webappsproject/entity/SystemUserGroup_.java
+++ target/generated-sources/annotations/com/webappsproject/entity/SystemUserGroup_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-04-26T13:16:30")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-04-26T16:00:49")
 @StaticMetamodel(SystemUserGroup.class)
 public class SystemUserGroup_ { 
 
