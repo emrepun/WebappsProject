f8f45cdcdf339869cc6d84574fcca735094ea22f
emrepun <emrehavan@hotmail.com>
2019-05-02 02:10:50 +0100

    Project renamed

--- target/generated-sources/annotations/com/webappsproject/entity/ProjectTopic_.java
+++ target/generated-sources/annotations/com/webappsproject/entity/ProjectTopic_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-05-02T01:36:54")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-05-02T02:09:34")
 @StaticMetamodel(ProjectTopic.class)
 public class ProjectTopic_ { 
 
